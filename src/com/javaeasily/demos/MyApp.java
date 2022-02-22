fffa0f1663a133ae6d425114a15ae4d05e324b94
Tawana Shava <tawanafshava@gmail.com>
2022-02-22 15:51:35 +0100

    Initial commit

---
+++ src/com/javaeasily/demos/MyApp.java
@@ -0,0 +1,14 @@
+package com.javaeasily.demos;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+	sayGreeting("Tawana");
+
+	}
+
+	private static void sayGreeting(String name) {
+		System.out.println("Hello"+ name);
+	}
+
+}
